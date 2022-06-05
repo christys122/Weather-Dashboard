330b6c730bc1980dfe74d692be89813dd9a0e2a5
unknown <mcsalazar122@gmail.com>
2022-06-04 22:58:36 -0500

    links to styles and api set up

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// points needed:
+// by city
+//current weather conditions - with icons
+// date
+// temp
+//humidity
+//wind speed
+//UV index - with icon change color based on favorable moderat or severe
+
+
+// Cards Section 5 day forcast 
+// date, icon, temp, wind speed, humidity
+
+//search history list of cities (calls data)
+
+//weather API call: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+//CALLBACK: https://api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid={API key}
+
+var getWeatherData = function() {
+    //fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=59c9c59cda310911670e4ae16ffa7819&units=imperial")
+};
+  
+  getWeatherData();
