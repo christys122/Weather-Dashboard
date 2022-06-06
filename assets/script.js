cc14d2d26bfb6b135c5df42fd6bbe71c22043f84
unknown <mcsalazar122@gmail.com>
2022-06-05 20:40:45 -0500

    search by city functional

--- assets/script.js
+++ assets/script.js
@@ -13,11 +13,54 @@
 
 //search history list of cities (calls data)
 
-//weather API call: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-//CALLBACK: https://api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid={API key}
+//weatherAPI call: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+var searchFormEl = document.querySelector("#searchForm")
+var cityInputEl = document.querySelector("#inputCity");
+var weatherContainerEl = document.querySelector(".weatherContainer");
 
-var getWeatherData = function() {
-    //fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=59c9c59cda310911670e4ae16ffa7819&units=imperial")
+//var city = document.getElementById("inputCity").value.trim();
+//console.log(city);
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+var city = cityInputEl.value.trim();
+  if (city) {
+    getWeatherData(city);
+    cityInputEl.textContent = "";
+
+  } else {
+    alert("Please enter a US city")
+  }
 };
+
+var getWeatherData = function(city1) {
+ // event.preventDefault();
+ var weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city1 + ",us&callback=test&appid=59c9c59cda310911670e4ae16ffa7819&units=imperial";
+
+
+    fetch(weatherApiUrl) //"https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=59c9c59cda310911670e4ae16ffa7819&units=imperial");
+      .then(function(response) {
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function(data) {
+            console.log(data);
+            displayWeather(data, city1)
+          });
+        } else {
+          alert("Error: city not found");
+        }
+      })
+      .catch(function(error) {
+        alert("Unable to connect to Weather")
+      });
+  };
   
-  getWeatherData();
+  var displayWeather = function(weather, searchTerm) {
+    if (weather.length === 0) {
+     weatherContainerEl.textContent ="not found";
+     return; 
+    }
+//format repo
+
+  };
+  //getWeatherData();
+  searchFormEl.addEventListener("submit", formSubmitHandler);
