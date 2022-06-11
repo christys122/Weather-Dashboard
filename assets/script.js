d2a3fda894e7b043c8a284f07bc28c343c80575c
unknown <mcsalazar122@gmail.com>
2022-06-10 22:24:35 -0500

    forecast pull and card setup

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,7 @@ var cityInputEl = document.querySelector("#inputCity");
 var weatherContainerEl = document.querySelector("#weatherContainer");
 var searchTermEl = document.querySelector("#returnCity");
 var listEl = document.querySelector("#winfo")
-
+var cardContainerEl = document.querySelector(".card-container")
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -49,8 +49,8 @@ var getWeatherData = function (city1) {
     })
     .then(function (weather) {
 
-      displayWeather(weather, city1);
-      
+      displayWeather(weather, city1 + " ");
+console.log(weather);
       coord(weather);
 
     })
@@ -69,11 +69,17 @@ var getWeatherData = function (city1) {
 };
 
 var displayWeather = function (weather, searchTerm) {
-  
+
 
 
   searchTermEl.textContent = searchTerm;
   //select each item using loop
+
+  var dateMain = weather.dt*1000;
+  var dateObject1 = new Date(dateMain)
+  var dateFormat = dateObject1.toLocaleString()
+  var dateEl = document.getElementById("date-main")
+  dateEl.textContent = "(" + dateFormat + ")";
 
 
   var tempData = weather.main.temp;
@@ -82,21 +88,21 @@ var displayWeather = function (weather, searchTerm) {
   tempEl.textContent = tempData;
   // wind
   var windData = weather.wind.speed;
-  
+
   var windEl = document.getElementById("wind");
   windEl.textContent = windData;
   //humid
   var humidityData = weather.main.humidity;
-  
+
   var humidityEl = document.getElementById("humidity");
   humidityEl.textContent = humidityData;
 
 
 
-}; 
-//fetch latlon
-var coord = function(weather) {
-  
+};
+//fetch coord
+var coord = function (weather) {
+
   //latlon
   var lat = weather.coord.lat;
   console.log(lat);
@@ -107,23 +113,80 @@ var coord = function(weather) {
 
   fetch(oneApi)
     .then(function (response) {
-      return response.json(); 
+      return response.json();
     })
-    .then(function(weather){
-      console.log(weather);
+    .then(function (weather) {
+      
       var uviData = weather.current.uvi;
-    
-  var uviEl = document.getElementById("uv");
-  uviEl.textContent = uviData;
-    })
-    
+
+      var uviEl = document.getElementById("uv");
+      uviEl.textContent = uviData;
+    });
+      //var forecastData = weather.daily.dt;
+      //console.log(forecastData);
   
-};
+var dayCastApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=59c9c59cda310911670e4ae16ffa7819&`
+
+fetch(dayCastApi)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+ 
+  var forecastData = data.daily;
+  console.log(forecastData);
 
 
+//for (var i =0; i < forecastData.length; i++) {
+  //var forecast = forecast[i].data.daily;
+  //console.log(forecast)
 
+  var displayForecast = function() {
+    if (forecastData.length === 0) {
+      cardContainerEl.textContent = "not found";
+      return;
+    }     
+    for (var i =0; i < forecastData.length; i++) {
+    var forecast = forecastData[i];
+     //console.log(forecastData);
+   
+  
+//transfer to cards
+  var dateData = forecastData[i].dt*1000;
+  var dateObject = new Date(dateData)
+  var dateFormat = dateObject.toLocaleString()
 
+  var cardDateEl = document.getElementById("card-date");
+  //var cardDateEl = document.getElementById("date2");
+  cardDateEl.textContent = dateFormat;
+  // wind
+  var iconData = forecastData[i].weather.icon;
+  var iconEl = document.getElementById("card-icon");
+  iconEl.textContent = iconData;
 
+  var cardTempData = forecastData[i].temp.day;
+
+  var cardTempEl = document.getElementById("card-temp");
+  cardTempEl.textContent = cardTempData;
+  // wind
+  var cardWindData = forecastData[i].weather.wind_speed;
+
+  var cardWindEl = document.getElementById("card-wind");
+  cardWindEl.textContent = cardWindData;
+  
+  //humid
+  var humidData = forecastData[i].humidity;
+
+  var humidEl = document.getElementById("humid");
+  humidEl.textContent = humidData;
+  
+}
+}
+
+  displayForecast();
+ }
+  
+)};
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
 
